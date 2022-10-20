95cec2c8b2b278c3a8db86001a5c67ee738b8bc4
Maksim Fedoseev <maxim.fedoseev13@gmail.com>
2022-10-21 00:51:42 +0300

    download lcov via pip

---
+++ cmake-build-debug/tests/CMakeFiles/test_toys_store.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maks/Desktop/questina/cpp_hw/toys_catalog/tests/tests.cpp" "/home/maks/Desktop/questina/cpp_hw/toys_catalog/cmake-build-debug/tests/CMakeFiles/test_toys_store.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../toys_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/maks/Desktop/questina/cpp_hw/toys_catalog/cmake-build-debug/toys_lib/CMakeFiles/toys_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

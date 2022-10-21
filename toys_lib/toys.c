4ed0053c5869a965e0ec17cf8fd010c2ed77a90e
questina <chrismatis123@gmail.com>
2022-10-21 14:41:27 +0300

    check realloc

--- toys_lib/toys.c
+++ toys_lib/toys.c
@@ -19,17 +19,20 @@ int add_toy(struct toy_array *store, char *toy_name, double toy_price,
       if (!store->toys){
           fprintf(stderr, "Cannot allocate memory for store");
           return -1;
-      } else {
-          store->toys[0] = new_toy;
-          store->size += 1;
       }
+      store->toys[0] = new_toy;
+      store->size += 1;
     } else {
-      store->toys =
+      struct toy *tmp_store = NULL;
+      tmp_store =
           realloc(store->toys, (store->size + 1) * sizeof(struct toy));
-      if (!store->toys){
+      if (!tmp_store){
+          free(store->toys);
+          store->toys = NULL;
           fprintf(stderr, "Cannot reallocate memory for store");
           return -1;
       } else {
+          store->toys = tmp_store;
           store->toys[store->size] = new_toy;
           store->size += 1;
       }
